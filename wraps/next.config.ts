22109b26731d483c7f81a8eb3af35940fa2ce724
Hyan <henriyan28@gmail.com>
2025-09-14 02:48:17 -0400

    osahdfosha

--- wraps/next.config.ts
+++ wraps/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
+  eslint: {
+    // ✅ Skip ESLint during builds on Vercel
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
